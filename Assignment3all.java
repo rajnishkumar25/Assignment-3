30b3f3f0fc78cf4794c8ca82736e19485ceb79c7
rajnishkumar25 <94224509+rajnishkumar25@users.noreply.github.com>
2021-11-19 16:44:10 +0530

    Add files via upload

---
+++ Assignment3all.java
@@ -0,0 +1,175 @@
+
+
+public class Assignment3all
+{  
+    public static void main(String[] args) 
+    // {
+    
+    //     System.out.println(" Hello Wordld");
+    
+    // }
+
+    // {
+    //     int i = 11;
+         
+    //     i = i++ + ++i + ++i + i++ + ++i;
+         
+    //     System.out.println(i);
+    // }
+
+    // {
+    //     int a=11, b=22, c;
+         
+    //     c = a + b + a++ + b++ + ++a + ++b;
+         
+    //     System.out.println("a="+a);
+    //     System.out.println("b="+b);
+    //     System.out.println("c="+c);
+
+    // }
+
+    // {
+    //     int i=0;
+         
+    //     i = i++ - --i + ++i - i--;
+         
+    //     System.out.println(i);
+         
+    // }
+    // {
+    //     boolean b = true;
+         
+    //     b++;
+         
+    //     System.out.println(b);
+    // }
+
+    // {
+    //     int i=1, j=2, k=3;
+         
+    //     int m = i-- - j-- - k--;
+         
+    //     System.out.println("i="+i);
+    //     System.out.println("j="+j);
+    //     System.out.println("k="+k);
+    //     System.out.println("m="+m);
+    // }
+
+    // {
+    //     int a=1, b=2;
+         
+    //     System.out.println(--b - ++a + ++b - --a);
+    // }
+
+    // {
+    //     int i = 11; 
+    //     int j = --i;
+    //     System.out.println(i);
+    //     System.out.println(j);
+
+    // }
+
+    // {
+    //     int m = 0, n = 0;
+         
+    //     int p = --m * --n * n-- * m--;
+         
+    //     		System.out.println(p);
+    // }
+
+    // {
+    //     int a=1;
+         
+    //     a = a++ + ++a * --a - a--;
+         
+    //     System.out.println(a);
+    // }
+
+    // {
+    //     int a = 11++;
+         
+    //     System.out.println(a);
+    // }
+
+    // {
+    //     double m=1010d, n=1010d;
+         
+    //     System.out.println(m++ / ++n * n-- / --m);
+    // }
+
+    // {
+    //     int x = 001, y=010, z=100;
+    //     System.out.println(y);
+         
+    //     int i = --x + y++ - z-- - --z + ++y - --x + y-- - --x;
+         
+    //     System.out.println("x="+x);
+    //     System.out.println("y="+y);
+    //     System.out.println("z="+z);
+    //     System.out.println("i="+i);
+    // }
+
+    // {
+    //     int x = 001, y=10, z=100;
+         
+    //     int i = y++ + ++y + y;
+         
+    //     System.out.println("x="+x);
+    //     System.out.println("y="+y);
+    //     System.out.println("z="+z);
+    //     System.out.println("i="+i);
+    // }
+
+        // {
+        //     int ch = 'B';
+             
+        //     System.out.println(++ch);
+        // }
+
+        // {
+        //    char ch = "123".charAt(1);
+          
+             
+        //     System.out.println(ch++ + ++ch);
+        //     System.out.println(ch);
+        //     System.out.println(ch++);
+        //     System.out.println(++ch);
+        // }
+
+        // {
+        //     char ch = '0';
+             
+        //     System.out.println(ch-- + --ch);
+        // }
+
+        // {
+        //     double d = 1.5, D = 2.5;
+             
+        //     System.out.println(d++ + ++D);
+        //        }
+
+        // {
+        //     int i = 0, j = 0;
+             
+        //     System.out.println(--i * i++ * ++j * j++);
+        // }
+    
+        {
+            char ch = 'A';
+             
+            System.out.println(++ch);
+        }
+    
+    
+    
+    
+    
+
+
+    
+
+
+
+
+
+}
